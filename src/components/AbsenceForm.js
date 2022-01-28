79a0e71ee70b13f62b9983f90a28197d4d0675c9
I-Zor <ivona.zoricic@gmail.com>
2022-01-28 13:51:06 +0100

    Added modal

--- src/components/AbsenceForm.js
+++ src/components/AbsenceForm.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
-import '../App.css';
+import '../styling/App.css';
+import '../styling/AbsenceForm.css';
 import axios from "axios";
+import Modal from 'react-modal';
+import swal from 'sweetalert2';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import sv from 'date-fns/locale/sv';
 registerLocale('sv', sv)
+
+Modal.setAppElement('#root');
 
 const AbsenceForm = (props) => {
 
@@ -16,13 +21,13 @@ const AbsenceForm = (props) => {
     };
 
     let chosenDate = startDate;
-    let convertedDate = chosenDate.toISOString().slice(0,10);
+    let convertedDate = chosenDate.toISOString().slice(0, 10);
 
     let childId = window.sessionStorage.getItem("childId");
 
     function setDateToToday(e) {
         if (e.target.checked) {
-             setStartDate(new Date());
+            setStartDate(new Date());
         };
     };
 
@@ -36,54 +41,77 @@ const AbsenceForm = (props) => {
         setReason(e.target.value)
     }
 
-    console.log(convertedDate)
+    function saveAbsence() {
+        if (reason === '') {
+            swal.fire({
+                icon: 'warning',
+                title: 'Vänligen ange anledning till frånvaro',
+              });
+        };
 
-     function saveAbsence() {
         let saveAbsenceUrl = 'http://localhost:8080/educator/absence/' + childId + '/' + convertedDate + '/' + reason;
-         axios.get(saveAbsenceUrl)
-             .then((response) => {
-                 let id = response.data;
-                 console.log(id);
-             })
-             .catch((error) => {
-                 console.log(error);
-             });
+        axios.get(saveAbsenceUrl)
+            .then(() => {
+                swal.fire({
+                    icon: 'success',
+                    title: 'Frånvaro är sparad',
+                  });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
 
     function handleSavingAbsence() {
         saveAbsence();
-    }
- 
+    };
+
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            transform: 'translate(-50%, -50%)',
+        },
+    };
+
     return (
-         <div className="modal">
-            <div id="absence-form">
-                <div id="date-today-form">
-                    <label>
-                        <input type="checkbox" id="checkbox-today" onChange={setDateToToday}/>
-                        Idag
-                    </label>
-                    <label>Eller välj annan dag (tryck på datumet nedanför)</label>
-                </div>
-                <div id="date-picker">
-                    <DatePicker locale="sv" selected={startDate} onChange={(date) => setStartDate(date)} />
-                </div>
-                <div id="reason-form">
-                    <form>
-                        <label> Anledning:
-                            <input type="text" id="reason" onChange={getReason}/>
+        <div>
+            <Modal
+                isOpen={props.show}
+                style={customStyles} >
+                <div id="absence-form">
+                    <div>
+                        <div id="date-today-form">
+                            <label>
+                                <input type="checkbox" id="checkbox-today" onChange={setDateToToday} />
+                                Idag
+                            </label>
+                            <label>Eller välj annan dag (tryck på datumet nedanför)</label>
+                        </div>
+                        <div id="date-picker">
+                            <DatePicker locale="sv" selected={startDate} onChange={(date) => setStartDate(date)} />
+                        </div>
+                        <div id="reason-form">
+                            <form>
+                                <label> Anledning:
+                                    <input type="text" id="reason" onChange={getReason} />
+                                </label>
+                            </form>
+                        </div>
+                        <label>
+                            <input type="checkbox" id="checkbox-offtime" onChange={setReasonAsDayOff} />
+                            Ledig
                         </label>
-                    </form>
+                    </div>
+                    <div className="buttons-modal">
+                        <button className="save-button" onClick={props.close}> Stäng</button>
+                        <button onClick={handleSavingAbsence} className="save-button">Spara</button>
+                    </div>
                 </div>
-                <label>
-                    <input type="checkbox" id="checkbox-offtime" onChange={setReasonAsDayOff} />
-                    Ledig
-                </label>
-                <div className="buttons">
-                <button onClick={handleSavingAbsence} className="save-button">Spara</button>
-                <button className="save-button" onClick={props.close}> Stäng</button>
-                </div>
-            </div>
-         </div>
+            </Modal>
+        </div>
     )
 }
 
--- src/components/CaringTimeForm.js
+++ src/components/CaringTimeForm.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
-import '../App.css';
 import TimePicker from 'react-time-picker';
 import axios from "axios";
+import Modal from 'react-modal';
+import '../App.css';
+import '../styling/CaringTimeForm.css';
+
+Modal.setAppElement('#root');
 
 const CaringTimeForm = (props) => {
 
@@ -9,7 +13,6 @@ const CaringTimeForm = (props) => {
     const [endValue, setEndValue] = useState('16:00');
     const [weekday, setWeekday] = useState('');
 
-    //    let chosenStartTime = startValue;
     let splittedStartTime = startValue.split(':');
     let startHour = splittedStartTime[0];
     let startMinute = splittedStartTime[1];
@@ -48,50 +51,65 @@ const CaringTimeForm = (props) => {
             });
     };
 
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            transform: 'translate(-50%, -50%)',
+        },
+    };
+
     return (
-        <div className="modal">
-            <div id="caring-time-form">
-                <h4>Välj veckodag</h4>
-                <div id="caring-time-picker">
-                    <div id="days-checkboxes">
-                        <label>
-                            <input type="radio" id="checkbox-monday" onChange={setWeekDay} value="Måndag" name="days" /> Måndag
-                        </label>
-                        <label>
-                            <input type="radio" id="checkbox-tuesday" onChange={setWeekDay} value="Tisdag" name="days"/> Tisdag
-                        </label>
-                        <label>
-                            <input type="radio" id="checkbox-wednesday" onChange={setWeekDay} value="Onsdag" name="days"/> Onsdag
-                        </label>
-                        <label>
-                            <input type="radio" id="checkbox-thursday" onChange={setWeekDay} value="Torsdag" name="days"/> Torsdag
-                        </label>
-                        <label>
-                            <input type="radio" id="checkbox-friday" onChange={setWeekDay} value="Fredag" name="days"/> Fredag
-                        </label>
+        <div>
+            <Modal
+                isOpen={props.show}
+                style={customStyles}
+            >
+                <div id="caring-time-form">
+                    <h4>Välj veckodag</h4>
+                    <div className="caring-time-picker">
+                        <div id="days-checkboxes">
+                            <label>
+                                <input type="radio" onChange={setWeekDay} value="Måndag" name="days" /> Måndag
+                            </label>
+                            <label>
+                                <input type="radio" onChange={setWeekDay} value="Tisdag" name="days" /> Tisdag
+                            </label>
+                            <label>
+                                <input type="radio" onChange={setWeekDay} value="Onsdag" name="days" /> Onsdag
+                            </label>
+                            <label>
+                                <input type="radio" onChange={setWeekDay} value="Torsdag" name="days" /> Torsdag
+                            </label>
+                            <label>
+                                <input type="radio" onChange={setWeekDay} value="Fredag" name="days" /> Fredag
+                            </label>
+                        </div>
+                        <div id="time-pickers">
+                            <div id="time-picker-start">
+                                <h4>Starttid</h4><br></br>
+                                <TimePicker
+                                    value={startValue}
+                                    onChange={setStartValue}
+                                    locale="sv" />
+                            </div>
+                            <div id="time-picker-end">
+                                <h4>Sluttid</h4><br></br>
+                                <TimePicker
+                                    value={endValue}
+                                    onChange={setEndValue}
+                                    locale="sv" />
+                            </div>
+                        </div>
                     </div>
-                    <div id="time-pickers">
-                        <div id="time-picker-start">
-                            <h4>Starttid</h4><br></br>
-                            <TimePicker
-                                value={startValue}
-                                onChange={setStartValue}
-                                locale="sv" />
-                        </div>
-                        <div id="time-picker-end">
-                            <h4>Sluttid</h4><br></br>
-                            <TimePicker
-                                value={endValue}
-                                onChange={setEndValue}
-                                locale="sv" />
-                        </div>
+                    <div className="buttons-modal">
+                        <button onClick={props.close} id="close-button">Stäng</button>
+                        <button onClick={changeCaringTime} className="save-button">Spara</button>
                     </div>
                 </div>
-                <div id="buttons-caring-time">
-                    <button onClick={props.close} id="close-button">Stäng</button>
-                    <button onClick={changeCaringTime} id="save-button-time-picker">Spara</button>
-                </div>
-            </div>
+            </Modal>
         </div>
     )
 }
